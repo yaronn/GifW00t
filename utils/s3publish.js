233855c698fc655811454d58d11ebc32711c6cc2
Yaron Naveh <yaronn01@yaronn01-VirtualBox.(none)>
2014-05-22 20:27:29 +0300

    fix deploy

--- utils/s3publish.js
+++ utils/s3publish.js
@@ -1,11 +1,23 @@
 var fs = require('fs')
 var AWS = require('aws-sdk');
+
+
+
+AWS.config.update({
+   'httpOptions': {
+      proxy: 'http://web-proxy.israel.hp.com:8080/'
+   },
+   sslEnabled: false
+});
+
+
 var s3 = new AWS.S3();
 var dir = require('./treedir');
 var mime = require('mime');
 var exec = require('child_process').exec;
 
 var folder = process.argv[2]
+
 
 function uploadFile(file) {
 
@@ -18,7 +30,7 @@ function uploadFile(file) {
            if (err) { throw err; }
             
            var obj = file.substring(folder.length+"/".length)
-        
+           console.log("before")
            s3.client.putObject({
                 Bucket: 'anigif100',
                 Key: obj,
@@ -27,6 +39,7 @@ function uploadFile(file) {
                 ContentType: mime.lookup(file),
                 ContentEncoding: "gzip"
                 }, function (err) {
+                    console.log("after")
                     if (err) {throw err; }
                     console.log(file + " --> " + obj)
                 })
@@ -35,7 +48,7 @@ function uploadFile(file) {
     });
 }
 
-
+console.log("start")
 dir.walk(folder, function(err, results) {
     for (var i=0; i<results.length; i++) {
         uploadFile(results[i])
